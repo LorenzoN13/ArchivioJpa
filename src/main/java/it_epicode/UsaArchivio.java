ba819ae3f93e4c0aedf0fc8d11131f8d99684da1
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-01-26 17:03:25 +0100

    fine

--- src/main/java/it_epicode/UsaArchivio.java
+++ src/main/java/it_epicode/UsaArchivio.java
@@ -18,33 +18,33 @@ public class UsaArchivio {
         utente.setNome("Giulio");
         utente.setCognome("Lefante");
         utente.setDataNascita(LocalDate.of(2000,04,23));
-        utente.setNumeroTessera("343574256251");
+        utente.setNumeroTessera("363583585426");
 
         archivioDAO.salvaUtente(utente);
 
         // Aggiunta di un libro
-        libro.setTitolo("Titolo rivista");
-        libro.setISBN("7355-5422-2452");
+        libro.setTitolo("giunto alla fine");
+        libro.setISBN("3564-3562-6866");
         libro.setNumeropagine(50);
-        libro.setAnnoPubblicazione(LocalDate.of(2000,12,13));
+        libro.setAnnoPubblicazione(LocalDate.of(2020,12,13));
         libro.setAuthor("Lorenzo Nicchia");
-        libro.setGenere(Genere.FANTASCENZA);
+        libro.setGenere(Genere.FANTASY);
 
         archivioDAO.aggiungiAllCatalogo(libro);
 
 
         // Aggiunta di una rivista
         rivista.setTitolo("Il Giornalino");
-        rivista.setISBN("3456-6834-5463");
+        rivista.setISBN("3436-6854-5855");
         rivista.setNumeropagine(20);
         rivista.setPeriodicita(Periodicita.SETTIMANALE);
-        rivista.setAnnoPubblicazione(LocalDate.of(2020,01,23));
+        rivista.setAnnoPubblicazione(LocalDate.of(2000,01,23));
 
         archivioDAO.salvaRivista(rivista);
 
 
         // Ricerca per ISBN
-        Catalogo libroTrovato = archivioDAO.ricercaPerISBN(3455-6364-5343);
+        Catalogo libroTrovato = archivioDAO.ricercaPerISBN(3436-6534-5875);
         System.out.println("Libro trovato: " + libroTrovato);
 
         // Ricerca per autore
@@ -52,14 +52,16 @@ public class UsaArchivio {
         System.out.println("Libri per autore: " + libriPerAutore);
 
         // Ricerca di tutti i prestiti scaduti e non ancora restituiti
-        List<Catalogo> prestitiScaduti = archivioDAO.getRicercaElementiInPrestito("124563424356");
+        List<Catalogo> prestitiScaduti = archivioDAO.getRicercaElementiInPrestito("1734763457445632");
         System.out.println("Prestiti scaduti: " + prestitiScaduti);
 
         // Ricerca degli elementi attualmente in prestito dato un numero di tessera utente
-        List<Catalogo> elementiInPrestito = archivioDAO.getRicercaElementiInPrestito("2835473467732");
-        System.out.println("Elementi attualmente in prestito per utente" + utente.getNumeroTessera() + ":");
+        List<Catalogo> elementiInPrestito = archivioDAO.getRicercaElementiInPrestito("1734763457445632");
+        System.out.println("Elementi attualmente in prestito per utente " + utente.getNumeroTessera() + ":");
         for (Catalogo c : elementiInPrestito) {
             System.out.println(c.getTitolo());
         }
+        System.out.println("L'elemento con l'ISBN" + libro.getISBN() + "è stato cancellato");
+        archivioDAO.rimuoviElementoCatalogo(3436-6534-5875);
     }
 }
